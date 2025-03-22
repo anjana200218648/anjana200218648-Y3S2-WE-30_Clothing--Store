ed55437dcdf264f71a6c4f65d192ec9b62059e2c
sachithn <sachithnimaliyanage@gmail.com>
2025-03-22 09:53:11 +0530

    add git2

--- backend/models/clothModel.js
+++ backend/models/clothModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-//add git
+
 const clothSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
