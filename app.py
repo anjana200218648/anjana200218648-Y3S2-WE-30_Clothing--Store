bce2c86e3dec313413ea4c9debb6a89303db0ebe
anjana200218648 <hashananjanahh3@gmail.com>
2025-04-27 14:03:33 +0530

    re m

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def train_and_save_model():
     try:
         df = pd.read_csv("sales_data.csv")
 
-        # 🔹 Convert 'Date' to datetime and create 'Month' column
+        # 🔹 Convert 'Date' to datetime and create 'Month'column
         df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
         df['Month'] = df['Date'].dt.to_period('M')  # Extract Year-Month
 
