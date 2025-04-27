b21a2c3fe719f65f35b22eb52efa050a58b16b57
sachithn <sachithnimaliyanage@gmail.com>
2025-04-27 19:38:18 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+    // Connect to MongoDB using mongoose
     await mongoose.connect('mongodb+srv://user1:123user@cluster0.zmxyl.mongodb.net/cloth-del').then(() => console.log("DB Connected"));
 }
