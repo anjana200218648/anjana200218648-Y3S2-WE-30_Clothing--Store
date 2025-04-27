65a8f2552aa27e45d8536820f25f704a775a0ef6
anjana200218648 <hashananjanahh3@gmail.com>
2025-04-27 14:27:03 +0530

    command

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def train_and_save_model():
 
          
         df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
-        df['Month'] = df['Date'].dt.to_period('M')  # E 
+        df['Month'] = df['Date'].dt.to_period('M')   
 
         # 🔹 Ensure 'Sales'  
         if 'Sales' not in df.columns:
