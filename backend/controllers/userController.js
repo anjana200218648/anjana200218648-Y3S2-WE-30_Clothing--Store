71bc13cdec1c28e4777c7b504881e54c5de1611b
sachithn <sachithnimaliyanage@gmail.com>
2025-04-27 18:26:39 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,11 +43,12 @@ const registerUser = async (req, res) => {
         if (exists) {
             return res.json({ success: false, message: "User already exists." })
         }
-        // validating email and strong password
+        // validating email 
         if (!validator.isEmail(email)) {
             return res.json({ success: false, message: "Please enter a valid email." })
         }
 
+        //validating strong password
         if (password.length < 8) {
             return res.json({ success: false, message: "Please enter a strong password." })
         }
