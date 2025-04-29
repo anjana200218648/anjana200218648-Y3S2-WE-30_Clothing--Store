b7ecc611a11e9f3a7f8c527ade6bd3884365a00b
yasindupavithra <yasi20000422@gmail.com>
2025-04-29 20:32:42 +0530

    LoginEmployee

--- routes/employee.js
+++ routes/employee.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginUser, saveUser } from "../controllers/userController.js";
+import { loginEmployee, saveUser } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
