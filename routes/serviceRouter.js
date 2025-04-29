ec241597a5c21d920a75a369512c4bf9ab84f5c2
yasindupavithra <yasi20000422@gmail.com>
2025-04-29 18:06:18 +0530

    serviceRouter new

---
+++ routes/serviceRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { loginUser, saveUser } from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/",saveUser)
+userRouter.post("/login",loginUser) //securiti   chek krannath post thama use kranne 
+
+export default userRouter;//serviceRouter new
