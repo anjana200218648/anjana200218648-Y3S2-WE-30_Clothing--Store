ee3e3c1026c83ba18511906427d9f5946c5f0da4
yasindupavithra <yasi20000422@gmail.com>
2025-04-29 17:52:58 +0530

    service.js start

---
+++ models/service.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema  ({
+     
+    email : {
+        type :String,
+        required : true,   //oonama   user  save krnakota  apita e mail eka oonani a nisa thmai meka daanne ........(innama oona ekak )
+        unique : true   //eka user knk ta ekai thiyuena oona nisa 
+
+
+    },
+    firstName : {
+        type : String,
+        required : true
+    },
+    LastName : {
+        type : String,
+        required : true
+    },
+    role : {
+        type : String,
+        required : true,
+        default : "user" //user=costomer  metthnadi      // e koiyanne karu hari role kiyana magaarala giyoth uuta   auto value ekak watenawa  
+
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    phone : {
+        type : String,
+        required : true,
+        default : "Not given"
+    },
+    idDisabled : {
+        type : Boolean,
+        required : true,
+        default : false
+    },
+    idEmailVerified : {
+        type : Boolean,
+        required : true,
+        default : false
+    },
+
+
+})
+const User = mongoose.model("user",userSchema)
+export default  User;
+
+//service.js 
